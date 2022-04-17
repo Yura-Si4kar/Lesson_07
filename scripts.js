533081888e6ba78e6aabbae763dd68c74cf5b7cc
Sichkar Yurii <96392712+Yura-Si4kar@users.noreply.github.com>
2022-04-17 19:18:11 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,33 +1,20 @@
-function createSum(a) {
-  return function(b) {
-    return a + b;
-  };
+function createCalculator(base) {
+  return {
+    sum: (b) => (base += b),
+    mult: (b) => (base *= b),
+    sub: (b) => (base -= b),
+    div: (b) => (base /= b),
+    set: (b) => (base = b),
+    get: () => base,
+  }
 };
 
-function createMultiply(a) {
-  return function (b) {
-    return a * b;
-  };
-};
+const calc = createCalculator(10);
 
-function createSabtract(a) {
-  return function (b) {
-    return b - a;
-  };
-};
+console.log (calc.sum(5));
+console.log (calc.mult(10));
+console.log (calc.sub(40));
+console.log(calc.div(10));
+calc.set(100);
 
-function createDivade(a) {
-  return function (b) {
-    return b / a;
-  };
-};
-
-const sum5 = createSum(5);
-const mult10 = createMultiply(10);
-const sub40 = createSabtract(40);
-const div10 = createDivade(10);
-
-console.log(`resultSum : ${sum5(10)}`);
-console.log(`resultMultiply : ${mult10(15)}`);
-console.log(`resultSubtract : ${sub40(150)}`);
-console.log(`resultDivide : ${div10(110)}`);
+console.log(calc.get());
